bbebcf76f6c234b61825b3d6b4f35c378a1d4cf0
Dylan McDonald <dmcdonald21@cnm.edu>
2018-12-14 11:24:49 -0800

    Great Scott! Completed time travel feature. This is heavy, Doc! :joy_cat:

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ class Game extends React.Component {
 	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if(calculateWinner(squares) || squares[i]) {
@@ -59,6 +59,7 @@ class Game extends React.Component {
 		squares[i] = this.state.xIsNext ? "X" : "O";
 		this.setState({
 			history: history.concat([{squares: squares}]),
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext
 		});
 	}
@@ -72,7 +73,7 @@ class Game extends React.Component {
 
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
 		const moves = history.map((step, move) => {
 			const desc = move ? "Go to move #" + move : "Go to start";

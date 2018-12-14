f677a641b54924d03fbd8ebe106238418da40429
Dylan McDonald <dmcdonald21@cnm.edu>
2018-12-14 09:41:56 -0800

    lifted up history a bit more

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null
-		};
-	}
-
 	render() {
 		return (
-			<button className="square" onClick={() => this.setState({value: "X"})}>
-			{this.state.value}
+			<button className="square" onClick={() => this.props.onClick()}>
+			{this.props.value}
 			</button>
 	);
 	}
@@ -27,8 +20,14 @@ class Board extends React.Component {
 		};
 	}
 
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = "X";
+		this.setState({squares: squares});
+	}
+
 	renderSquare(i) {
-		return <Square value={this.state.squares[i]} />;
+		return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
 	}
 
 	render() {

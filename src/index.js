03a835eb16159fe4658a47c697ce3092f6b8c9ca
Dylan McDonald <dmcdonald21@cnm.edu>
2018-12-14 10:43:19 -0800

    lifted up state to game

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true
-		};
-	}
-
 	handleClick(i) {
 		const squares = this.state.squares.slice();
 		if(calculateWinner(squares) || squares[i]) {
@@ -32,7 +24,7 @@ class Board extends React.Component {
 	}
 
 	renderSquare(i) {
-		return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+		return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
 	}
 
 	render() {
@@ -68,6 +60,14 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			history: [{squares: Array(9).fill(null)}],
+			xIsNext: true
+		};
+	}
+
 	render() {
 		return (
 			<div className="game">
